9782590b713f2124fc340e6ba7e62af36f38a736
Josef Bouska <bow.ska@seznam.cz>
2017-03-29 16:17:24 +0200

    tensorflow camera clasiffier - wip 5

--- tf-classifier/classifier.py
+++ tf-classifier/classifier.py
@@ -7,25 +7,32 @@ from PIL import Image
 import multilayer_perceptron as mp
 from random import shuffle
 from time import sleep
+import sys
+
+#TODO napad:
+# dalo by se rozdelit obrazek na sekce a v tech spocitat pocet rozdilnych pixelu.
+# Zmensilo by to pocet vstupu, ale neztratilo by se tolik informace, jako pri prostem zmenseni!
+# Byl by to takovy lepsi resize
 
 # Parameters
-learning_rate = 0.001
+learning_rate = 0.0001
 training_epochs = 10000
 batch_size = 50
 eval_step = 10
-save_step = 100
+save_step = 1000
 
-# zatim nejlepsi vysledky mi dava obrazek/10, FUZZ 10, sit 1024x32
+# zatim nejlepsi vysledky mi dava FUZZ=10, obrazek/10, sit 256x32
 # input image parameters
 FUZZ = 10
-image_size_x = 1920 / 10
-image_size_y = 1080 / 10
+img_resize = 10
+image_size_x = 1920 / img_resize
+image_size_y = 1080 / img_resize
 channels = 1 # R,G,B = 3 B/W = 1
 
 # Network Parameters
 # TODO jaka je optimalni velikost pro danou ulohu a velikost dat?
-n_hidden_1 = 256 # 1st layer number of features #24
-n_hidden_2 = 16 # 2nd layer number of features  #16
+n_hidden_1 = 64 # 1st layer number of features #24
+n_hidden_2 = 32 # 2nd layer number of features  #16
 n_input = image_size_x * image_size_y * channels # MNIST data input
 n_classes = 2 # MNIST total classes (negative alarm, positive alarm) (pocet vystupu ze site)
 
@@ -36,6 +43,10 @@ n_classes = 2 # MNIST total classes (negative alarm, positive alarm) (pocet vyst
 data_path = "/home/pepa/projects/camera_filter/learning/diff%s-%s" % (FUZZ, image_size_x)
 n_test_pct = 10 # procent testovacich dat
 
+model_name = "model-%s-%s-%s-%s" % (FUZZ, image_resize, n_hidden_1, n_hidden_2)
+
+x = tf.placeholder(tf.float32, shape=(None, n_input))
+y = tf.placeholder(tf.int64, shape=(None))
 
 def get_files(path):
     print "path = %s" % (path, )
@@ -59,11 +70,11 @@ def get_images_labels(files):
     for filename in files:
         # podle prviho pismena posledni slozky souboru pozname true/false
         if filename.split("/")[-2:][0][0] == 'f': #false
-            #TODO vahy pro true false?
-            labels.append(np.array((1, 0))) #negative alarm
+            #TODO pravdepodobnost pro true false?
+            labels.append(0) #negative alarm
             #print ("%s (0)" % (filename, ))
         else:
-            labels.append(np.array((0, 2))) #positive alarm (ma vyssi vahu?)
+            labels.append(1) #positive alarm (ma vyssi vahu?)
             #print ("%s (1)" % (filename, ))
         image = Image.open(filename)
         # TODO if image.size <> network size?
@@ -74,148 +85,102 @@ def get_images_labels(files):
     np_labels = np.array(labels)
     return np_images, np_labels
 
-# nacist data
-#   p = subprocess.Popen(["find %s -type f | sort -R" % (data_path)], stdout=subprocess.PIPE, shell=True)
-#   (output, err) = p.communicate()
-#   p_status = p.wait()
-#   files = output.split()
-#   input_images = []
-#   input_labels = []
-#   n_examples = len(files)
-#   n_test = int(n_examples / 100) * n_test_pct
-#   n_train = n_examples - n_test
-#   for filename in files:
-#     # TODO udelat lepe rozpoznani true/false
-#     # /home/pepa/projects/tensorflow/pokus1/data/true/ARC20170318095401-diff.pn
-#     if filename[43] == 'f': #false
-#TODO vahy true false?
-#       input_labels.append(np.array((1, 0))) #negative alarm
-#       #print ("%s (0)" % (filename, ))
-#     else:
-#       input_labels.append(np.array((0, 1))) #positive alarm
-#       #print ("%s (1)" % (filename, ))
-#     image = Image.open(filename)
-#     #image = image.resize((image_size_x, image_size_y))
-#     input_images.append(np.array(image))
-# TODO rozdelit vstupni data na trenovaci a testovaci mnozinu
-# TODO nahodne zamichat pro kazdou epochu
-#   train_images = np.array(input_images[:n_train])
-#   train_images = train_images.reshape(n_train, n_input)
-#   train_labels = np.array(input_labels[:n_train])
-#   test_images = np.array(input_images[n_train:])
-#   test_images = test_images.reshape(n_test, n_input)
-#   test_labels = np.array(input_labels[n_train:])
-#train_labels = train_labels.reshape(n_train, n_classes) # neni treba, jiz je spravne
+def main(argv):
 
-# tf Graph input
-# FIXME float nebo int?
-x = tf.placeholder(tf.float32, shape=(None, n_input))
-y = tf.placeholder(tf.float32, shape=(None, n_classes))
+    # Construct model
+    multilayer_perceptron = mp.MultilayerPerceptron(n_input, n_hidden_1, n_hidden_2, n_classes)
+    pred = multilayer_perceptron.get_model(x)
 
+    # Define loss function
+    cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=y)) #label je index spravneho vystupu
+    # Define optimizer
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost, global_step=global_step)
+    #TODO vyzkouset jine optimalizace
+    #optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost, global_step=global_step)
 
-# Construct model
-multilayer_perceptron = mp.MultilayerPerceptron(n_input, n_hidden_1, n_hidden_2, n_classes)
-pred = multilayer_perceptron.get_model(x)
-
-#TODO vyzkouset jine optimalizace
-# Define loss and optimizer
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-#labels = tf.to_int64(labels)
-#cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-#  logits=pred, labels=y, name='xentropy')
-#cost = tf.reduce_mean(cross_entropy, name='xentropy_mean')
-#global_step = tf.Variable(0, name='global_step', trainable=False)
-#optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost, global_step=global_step)
-
-# Initializing the variables
-init = tf.global_variables_initializer()
-
-# Launch the graph
-with tf.Session() as sess:
-    sess.run(init)
+    # Initializing the variables
+    init = tf.global_variables_initializer()
 
     # Test model
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    correct_prediction = tf.equal(tf.argmax(pred, 1), y)
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    #correct_prediction = tf.nn.in_top_k(tf.argmax(pred, 1), tf.argmax(y, 1), 1)
-    #accuracy = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
 
     # Create a saver for writing training checkpoints.
     saver = tf.train.Saver()
 
-    train_files, test_files = get_files(data_path)
-    n_train = len(train_files)
-    test_images, test_labels = get_images_labels(test_files)
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
 
-    best_acc = 0
-    # Training cycle
-    for epoch in range(training_epochs):
-        shuffle(train_files)
-        # TODO nejak se mi nelibi ze pro kazdou epochu musim znovu nacitat obrazky (ale zase to setri pamet)
-        # je to tu hlavne kvuli moznosti zamichat testovaci data pro kazdou epochu
-        train_images, train_labels = get_images_labels(train_files)
-        avg_cost = 0.
-        index_in_epoch = 0
-        total_batches = int(n_train/batch_size)
-        # pokud data nejsou delitelna davkou, chcem pouzit i zbytek
-        if n_train > total_batches * batch_size:
-            total_batches += 1
-        # Loop over all batches
-        for i in range(total_batches):
-            _, c = sess.run([optimizer, cost], feed_dict={x: train_images[index_in_epoch : index_in_epoch+batch_size],
-                                                          y: train_labels[index_in_epoch : index_in_epoch+batch_size]})
-            # Compute average loss
-            avg_cost += c / total_batches
-            index_in_epoch += batch_size
+        train_files, test_files = get_files(data_path)
+        n_train = len(train_files)
+        test_images, test_labels = get_images_labels(test_files)
 
-        # Display logs per epoch step
-        print("Epoch:", '%04d' % (epoch+1), "cost=", \
-            "{:.9f}".format(avg_cost))
-        if epoch % eval_step == 0:
-            train_acc = accuracy.eval({x: train_images, y: train_labels})
-            test_acc = accuracy.eval({x: test_images, y: test_labels})
-            print("Accuracy on train images:", train_acc)
-            print("Accuracy on test images:", test_acc)
-            if train_acc > best_acc:
-                best_acc = train_acc
-                saver.save(sess, 'model', global_step=epoch)
-        if epoch % save_step == 0:
-            saver.save(sess, 'model', global_step=epoch)
-        # TODO toto je tu kvuli snizeni vytizeni procesoru
-        sleep(1)
-    print("Optimization Finished!")
+        best_acc = 0
+        # Training cycle
+        for epoch in range(training_epochs):
+            shuffle(train_files)
+            # TODO nejak se mi nelibi ze pro kazdou epochu musim znovu nacitat obrazky (ale zase to setri pamet)
+            # je to tu hlavne kvuli moznosti zamichat testovaci data pro kazdou epochu
+            train_images, train_labels = get_images_labels(train_files)
+            avg_cost = 0.
+            index_in_epoch = 0
+            total_batches = int(n_train/batch_size)
+            # pokud data nejsou delitelna davkou, chcem pouzit i zbytek
+            if n_train > total_batches * batch_size:
+                total_batches += 1
+            # Loop over all batches
+            for i in range(total_batches):
+                _, c = sess.run([optimizer, cost], feed_dict={x: train_images[index_in_epoch : index_in_epoch+batch_size],
+                                                              y: train_labels[index_in_epoch : index_in_epoch+batch_size]})
+                # Compute average loss
+                avg_cost += c / total_batches
+                index_in_epoch += batch_size
 
-    # TODO ukladat do nazvu modelu parametry
-    saver.save(sess, 'model')
+            # Display logs per epoch step
+            print("Epoch:", '%04d' % (epoch), "cost=", \
+                "{:.9f}".format(avg_cost))
+            if epoch % eval_step == 0:
+                train_acc = accuracy.eval({x: train_images, y: train_labels})
+                test_acc = accuracy.eval({x: test_images, y: test_labels})
+                print("Accuracy on train images:", train_acc)
+                print("Accuracy on test images:", test_acc)
+                if train_acc > best_acc:
+                    best_acc = train_acc
+                    saver.save(sess, "./" + model_name, global_step=epoch)
+            if epoch % save_step == 0:
+                saver.save(sess, "./" + model_name, global_step=epoch)
+            # toto je tu zamerne, kvuli snizeni vytizeni procesoru
+            sleep(1)
+        print("Optimization Finished!")
 
-    # TODO zkouska na konkretnim obrazku
-    #cl = pred.eval(feed_dict={x: [mnist.test.images[0]]})
-    print "Prediction mismatches in test data:"
-    dt = 0
-    df = 0
-    mt = 0
-    mf = 0
-    for n, test_image in enumerate(test_images):
-        cl = sess.run(tf.argmax(pred, 1), feed_dict={x: [test_image]})
-        # tf.argmax(pred, 1) vrati index vyssiho cisla z pole vystupnich neuronu, tedy 0 pro false alarm a 1 pro true alarm
-        # v test_labels[n] je (True, False) pro false alarm a (False, True) pro true alarm
-        label = 1 if test_labels[n][1] else 0
-        #print cl, test_labels[n], label
-        if label:
-            dt = dt + 1
-        else:
-            df = df + 1
-        if cl != label:
-            print "Prediction: %s, label: %s, test file: %s" % (cl, label, test_files[n])
+        saver.save(sess, "./" + model_name)
+
+        print "Prediction mismatches in test data:"
+        dt = 0
+        df = 0
+        mt = 0
+        mf = 0
+        for n, test_image in enumerate(test_images):
+            cl = sess.run(tf.argmax(pred, 1), feed_dict={x: [test_image]})
+            label = test_labels[n]
             if label:
-                mt = mt + 1
+                dt = dt + 1
             else:
-                mf = mf + 1
-    print "%s/%s true and %s/%s false mismatches" % (mt, dt, mf, df)
+                df = df + 1
+            if cl != label:
+                print "Prediction: %s, label: %s, test file: %s" % (cl, label, test_files[n])
+                if label:
+                    mt = mt + 1
+                else:
+                    mf = mf + 1
+        print "%s/%s true and %s/%s false mismatches" % (mt, dt, mf, df)
 
-    train_images, train_labels = get_images_labels(train_files)
-    print("Accuracy on train images:", accuracy.eval({x: train_images, y: train_labels}))
-    print("Accuracy on test images:", accuracy.eval({x: test_images, y: test_labels}))
+        train_images, train_labels = get_images_labels(train_files)
+        print("Accuracy on train images:", accuracy.eval({x: train_images, y: train_labels}))
+        print("Accuracy on test images:", accuracy.eval({x: test_images, y: test_labels}))
 
+if __name__ == "__main__":
+    main(sys.argv[1:])
